810733b015d7bc278be8c602de414364e13c4342
jinricheng <jinricheng123@gmail.com>
2015-03-27 13:12:08 +0100

    first design of activity done

--- app/src/main/java/myaplication/tfg/org/myapplication/MainActivity.java
+++ app/src/main/java/myaplication/tfg/org/myapplication/MainActivity.java
@@ -1,24 +1,34 @@
 package myaplication.tfg.org.myapplication;
 
+import android.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.AlphaAnimation;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    TextView txtView = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        AlphaAnimation fadeIn = new AlphaAnimation(0.0f , 1.0f ) ;
+
+        txtView = (TextView)findViewById(R.id.titul);
+        txtView.startAnimation(fadeIn);
+        fadeIn.setDuration(1200);
+        fadeIn.setFillAfter(true);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+     //   getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -30,9 +40,7 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
